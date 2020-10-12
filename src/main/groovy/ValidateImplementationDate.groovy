f467b04a3c1c367c67ed306dad81dcebdb197810
nsbassi <navtej.bassi@insightplm.com>
2020-10-12 17:52:30 +0530

    WIP

---
+++ src/main/groovy/ValidateImplementationDate.groovy
@@ -0,0 +1,31 @@
+import com.agile.agileDSL.ScriptObj.IBaseScriptObj
+import com.agile.api.ICell
+import com.agile.api.IChange
+import com.agile.px.IUpdateEventInfo
+
+import java.util.logging.Logger
+
+void invokeScript(IBaseScriptObj obj) {
+    IUpdateEventInfo eventInfo = obj.PXEventInfo
+    Logger logger = Logger.getLogger('insight.sun.ams.ValidateImplementationDate')
+
+    IChange change = eventInfo.dataObject
+    Date createDate = change.getValue('dateOriginated')
+
+    logger.info("Creation date for AAS $change.name is $createDate")
+
+    def implDate = createDate + 30
+    logger.info("Implementation date for AAS $change.name should be on or after $implDate")
+
+    Integer atrId =  change.agileClass.getAttribute('Page Three.Implementation Schedule Date').id
+    ICell dirtyCell = eventInfo.getCell(atrId)
+
+    if(dirtyCell) {
+        Date newImplDate =dirtyCell.getValue()
+        logger.info("New Implementation date for AAS $change.name is being set to $newImplDate")
+
+        if (implDate >= newImplDate) {
+            throw new Exception("Implementation date should be at least one month after AAS creation date")
+        }
+    }
+}
